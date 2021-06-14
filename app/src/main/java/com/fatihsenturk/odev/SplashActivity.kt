f23975a2abce09252796941a6fd38b58deb378ed
senturkfatih03 <senturkfatih03@outlook.com>
2021-06-14 23:05:15 +0300

    Mobil Donem Odevi

--- app/src/main/java/com/fatihsenturk/odev/SplashActivity.kt
+++ app/src/main/java/com/fatihsenturk/odev/SplashActivity.kt
@@ -6,9 +6,15 @@ import android.os.Bundle
 import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    private var splashActivitytimeout : Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, splashActivitytimeout)
     }
 
     private fun loadSplashScreen(){
@@ -18,3 +24,4 @@ class SplashActivity : AppCompatActivity() {
     }
 
 }
+
